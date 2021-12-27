a323afd6cea695983c4c530d4a7a25fb5a3ec990
Winston <winstonmdp@gmail.com>
2021-12-28 00:07:51 +0300

    add(Charts.cpp): add Collection subcluss - VectorCollection - and its Iterator - VectorIterator

--- Charts.cpp
+++ Charts.cpp
@@ -2,11 +2,24 @@
 #include <string>
 #include <functional>
 
-template <class Element>
+template <class Element, class Update>
 class Collection
 {
 	public:
-	virtual Collection <Element>* collectionWithNewElement (Element*) = 0;
+	virtual Iterator <Collection <Element, Update>, Element, Update> iterator() = 0;
+};
+
+template <class Element, class Update>
+class VectorCollection : public Collection <Element, Update>
+{
+	public:
+	Iterator <Collection <Element, Update>, Element, Update> iterator()
+	{
+		return new VectorIterator (_vector);
+	}
+
+	private:
+	std::vector <Element>* _vector;
 };
 
 template <class Collection, class Element, class Update>
@@ -19,6 +32,64 @@ class Iterator
 	virtual Element* current() = 0;
 	virtual void updateCurrentToNewCollection (Update*) = 0;
 	virtual Iterator <Collection, Element, Update>* updatedCollectionIterator() = 0;
+};
+
+template <class Element, class Update>
+class VectorIterator : public Iterator <Collection <Element, Update>, Element, Update>
+{
+	public:
+	VectorIterator (std::vector <Element>* vector)
+	: _vector {vector}
+	{
+	}
+
+	void first() override
+	{
+		if (isUpdated) {
+			throw "It collection has already updated. We use immutable way.";
+		}
+
+		if (_vector->size() > 0) {
+			_currentIndex = 0;
+		}
+		else {
+			throw "It doesn`t have elements.";
+		}
+	}
+
+	bool isDone() override
+	{
+		return _currentIndex == _vector->size();
+	}
+
+	void next() override
+	{
+		++_currentIndex;
+		if (_currentIndex > _vector->size()) {
+			throw "_CurrentIndex > vector.size()";
+		}
+	}
+
+	Element* current() override
+	{
+		return _vector.at (_currentIndex);
+	}
+
+	void updateCurrentToNewCollection (Update* update) override
+	{
+		_vector->at (_currentIndex)->update (update);
+		_isUpdated = true;
+	}
+
+	Iterator <Collection <Element, Update>, Element, Update>* updatedCollecitonIterator()
+	{
+		return new VectorIterator (_vector);
+	}
+
+	private:
+	std::vector <Element>* _vector;
+	long _currentIndex;
+	bool _isUpdated = false;
 };
 
 template <class Iterator, class Function>
@@ -35,17 +106,13 @@ class ChartPoint
 	virtual std::string* valueName() = 0;
 	virtual double numberValue() = 0;
 	virtual App* app() = 0;
+	virtual void update() = 0;
 };
 
-class ChartPointsIterator
+class ChartPointsIterator : public Iterator <Collection <ChartPoint, Event>, ChartPoint, Event>
 {
 	public:
-	virtual Iterator <Collection <ChartPoint>, ChartPoint, Event> generalizedIterator() = 0;
-	virtual void first() = 0;
-	virtual bool isDone() = 0;
-	virtual void next() = 0;
-	virtual ChartPoint* current() = 0;
-	virtual void updateCurrentChartPointsToNewChartPoints (Event*) = 0;
+	virtual void updateCurrentChartPointToNewChartPoints (Event*) = 0;
 	virtual ChartPointsIterator* updatedChartPointsIterator() = 0;
 };
 
@@ -60,17 +127,19 @@ class Chart
 class ScreenTimeAllAppsMounth : public Chart
 {
 	public:
-	ScreenTimeAllAppsMounth (std::string* name, Collection <ChartPoint>* chartPoints)
-	: _name {name}, _chartPoints {chartPoints}
+	ScreenTimeAllAppsMounth (std::string* name, Collection <ChartPoint, Event>* chartPoints)
+	: _name {name}, _chartPoints {chartPoints}, _chartPointsIterator {new ChartPointsIterator (chartPoints)}
 	{
 	}
 
 	ScreenTimeAllAppsMounth* updatedChart (Event* event) override
 	{
 		forEach (_chartPointsIterator,
-		         [event] (ChartPointsIterator* chartPointsIterator) {chartPointsIterator->updateCurrentChartPointsToNewChartPoints(event);}
+		        [event] (ChartPointsIterator* chartPointsIterator)
+				{chartPointsIterator->updateCurrentChartPointToNewChartPoints (event);}
 		);
-		return new ScreenTimeAllAppsMounth (_name, _chartPointsIterator);
+		ChartPointsIterator* updatedChartPointsIterator = _chartPointsIterator->updatedChartPointsIterator();
+		return new ScreenTimeAllAppsMounth (_name, updatedChartPointsIterator);
 	}
 
 	std::string* name() override
@@ -85,18 +154,13 @@ class ScreenTimeAllAppsMounth : public Chart
 
 	private:
 	std::string* _name;
-	Collection <ChartPoint>* _chartPoints;
+	Collection <ChartPoint, Event>* _chartPoints;
 	ChartPointsIterator* _chartPointsIterator;
 };
 
-class ChartsIterator
+class ChartsIterator : public Iterator <Collection <Chart, Event>, Chart, Event>
 {
 	public:
-	virtual Iterator <Collection <Chart>, Chart, Event> generalizedIterator() = 0;
-	virtual void first() = 0;
-	virtual bool isDone() = 0;
-	virtual void next() = 0;
-	virtual Chart* current() = 0;
 	virtual void updateCurrentChartsToNewCharts (Event*) = 0;
 	virtual ChartsIterator* updatedChartsIterator() = 0;
 };
@@ -157,11 +221,13 @@ class Main
 		while (true) {
 			Event* event = _userPC->event();
 			forEach (_chartsIterator,
-			         [event] (ChartsIterator* chartsIterator) {chartsIterator->updateCurrentChartsToNewCharts (event);}
+			        [event] (ChartsIterator* chartsIterator)
+					{chartsIterator->updateCurrentChartsToNewCharts (event);}
 			);
 			ChartsIterator* updatedChartsIterator = _chartsIterator->updatedChartsIterator();
 			forEach (updatedChartsIterator,
-			         [_thisAppWindow] (ChartsIterator* updatedChartsIterator) {_thisAppWindow->print (updatedChartsIterator->current());}
+			        [_thisAppWindow] (ChartsIterator* updatedChartsIterator)
+					{_thisAppWindow->print (updatedChartsIterator->current());}
 			);
 		}
 	}
